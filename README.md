e2885e60375934ffc0be636e9989fe59bbb80663
Eetay Natan <eetay.natan@samanage.com>
2018-04-04 16:17:48 +0300

    added start-api.sh; removed old deploy script

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ```./bin/deploy.sh lambda1 --alias=PROD --function=3```
 ```./bin/publish.sh lambda1```
 ```./bin/stack-up.sh lambda1```
+
+brew install docker docker-machine
+brew install yarn --without node
+
--- bin/invoke.sh
+++ bin/invoke.sh
@@ -2,15 +2,29 @@
 AWS_SDK_LOAD_CONFIG=1
 AWS_REGION=us-east-1
 PROFILE=samanage-sandbox
+SCRIPT=`basename $0`
 FUNC="$1"
+if [ -z "$FUNC" ]; then
+  echo "Usage: $SCRIPT <lambda-name> "
+  exit 1
+fi
 #LOG_PARAMS="--log-file ./output.log"
 FUNCROOT=packages/$FUNC
+yarn --cwd $FUNCROOT
 if [ ! -f $FUNCROOT/index.js ]; then
   echo "Wrong folder $FUNCROOT"
   exit 1
 fi
 STDPARAMS="-v $FUNCROOT -t sam/template.yaml --profile=$PROFILE"
-COMMAND="sam local invoke $FUNC -v $FUNCROOT $STDPARAMS ${@:2} --parameter-values CodeUri=$FUNCROOT"
-COMMAND="sam local start-api $STDPARAMS ${@:2}"
+
+case $SCRIPT in
+start-api.sh)
+  COMMAND="sam local start-api $STDPARAMS ${@:2}"
+  ;;
+*)
+  COMMAND="sam local invoke $FUNC -v $FUNCROOT $STDPARAMS ${@:2} --parameter-values CodeUri=$FUNCROOT"
+  ;;
+esac
+
 echo "Executing: $COMMAND"
 $COMMAND
