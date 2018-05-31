7a37a4ed2f678a7b0437466c78b4eac41cebe02f
Eetay Natan <eetay.natan@samanage.com>
2018-05-31 15:15:29 +0300

    Added support for subnets and security group config of AuthorizerLambdaFunction

--- bin/cim.sh
+++ bin/cim.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-AWS_SDK_LOAD_CONFIG=1
+MYDIR=`dirname $0`
+export AWS_SDK_LOAD_CONFIG=1
+export AWS_REGION=us-east-1
+export AWS_PROFILE=samanage-sandbox
+export AWS_DEFAULT_VPC=`aws ec2 describe-vpcs --filters='{"Name":"isDefault","Values":["true"]}' --output text --query 'Vpcs[0].VpcId'`
+export AWS_DEFAULT_VPC_SUBNETS=`aws ec2 describe-subnets --filters='{"Name":"vpc-id","Values":["'$AWS_DEFAULT_VPC'"]}' --output text --query 'Subnets[*].SubnetId' |  sed 's/\t/,/g'`
+export | grep AWS | sed 's/^.*AWS/AWS/g'
+#
 HOST_IP=`ifconfig | grep 192.168.1 | head -1 | sed 's/ net.*$//;s/^.* 192/192/'`
-AWS_REGION=us-east-1
 FUNC=`basename "$1"`
 DIR_PARAMS="--dir packages/$FUNC"
-ALL_PARAMS="$DIR_PARAMS --profile=samanage-sandbox --debug=yes"
+ALL_PARAMS="$DIR_PARAMS --profile=$AWS_PROFILE --debug=yes"
 CIM="../cim/bin/cim"
 SCRIPT=`basename $0`
 case $SCRIPT in
